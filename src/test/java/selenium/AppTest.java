c0eed9172f535a20cd3aa16a510fbb4e26a8fd52
Eduardo Cabrera <lalo@eof.cl>
2022-03-16 22:30:35 -0300

    test chrome remoto

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -7,8 +7,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import org.junit.Test;
 import org.junit.Before;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Unit test for simple App.
@@ -16,18 +23,33 @@ import org.junit.Before;
 public class AppTest 
 {
     
-    private WebDriver driver;
+    private RemoteWebDriver driver;
 
     @Before
-    public void setUp(){
+    public void setUp() throws MalformedURLException, InterruptedException {
         System.out.println("Iniciando configuración...");
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
-        driver = new ChromeDriver();
-        driver.get("https://www.amazon.com");
+        //System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName("chrome");
+        //cap.setPlatform(Platform.WIN10);
+
+        ChromeOptions options = new ChromeOptions();
+        options.merge(cap);
+
+        String huburl = "http://localhost:4445/wd/hub";
+        driver = new RemoteWebDriver(new URL(huburl), options);
+
         driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        //driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        //driver.get("https://www.amazon.com");
+        //driver.manage().window().maximize();
+        driver.navigate().to("https://www.google.com");
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
-        driver.navigate().to("https://www.google.com");
     }
 
     @Test
@@ -37,6 +59,6 @@ public class AppTest
         WebElement searchbox = driver.findElement(By.name("q"));
         searchbox.sendKeys("HandBook Devops");
         searchbox.submit();
-        assertEquals("HandBook Devops", driver.getTitle());
+        assertEquals("HandBook Devops - Buscar con Google", driver.getTitle());
     }
 }
