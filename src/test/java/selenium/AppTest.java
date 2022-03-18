6868f33fd2af56b116c4cf9853a67c822be73cef
Gonzalo Muñoz Boisier <gmboisier@gmail.com>
2022-03-18 15:54:34 -0300

    Agrega las pruebas para selenium

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -1,42 +1,67 @@
 package selenium;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.Test;
 import org.junit.Before;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    
+public class AppTest {
+
     private WebDriver driver;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         System.out.println("Iniciando configuración...");
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
-        driver = new ChromeDriver();
-        driver.get("https://www.amazon.com");
-        driver.manage().window().maximize();
-        System.out.println(driver.getCurrentUrl());
-        System.out.println(driver.getTitle());
-        driver.navigate().to("https://www.google.com");
+        System.setProperty("webdriver.chrome.driver", "/home/gonzalo/ejemploSeleniumWeb/drivers/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        driver = new ChromeDriver(options);
     }
 
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        System.out.println("Iniciando Pruebas...");
+    public void busquedaDevOpsHandbook() {
+        driver.navigate().to("https://www.google.com");
         WebElement searchbox = driver.findElement(By.name("q"));
         searchbox.sendKeys("HandBook Devops");
         searchbox.submit();
-        assertEquals("HandBook Devops", driver.getTitle());
+        WebDriverWait wait1 = new WebDriverWait(driver, 20);
+        WebElement element = wait1.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(
+                "#rso a"))));
+
+        element.click();
+        WebDriverWait wait = new WebDriverWait(driver, 50);
+        WebElement productTitle = wait
+                .until(ExpectedConditions.visibilityOf(driver.findElement(By.id("productTitle"))));
+
+        assertEquals(
+                "The DevOps Handbook: How to Create World-Class Agility, Reliability, and Security in Technology Organizations",
+                productTitle.getText());
+    }
+
+    @Test
+    public void busquedaThePhoenixProject() {
+        driver.navigate().to("https://www.amazon.com");
+        WebElement searchbox = driver.findElement(By.name("field-keywords"));
+        searchbox.sendKeys("The Phoenix Project");
+        searchbox.submit();
+        WebDriverWait wait1 = new WebDriverWait(driver, 20);
+        WebElement element = wait1.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(
+                ".widgetId\\=search-results_1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1) > span:nth-child(1)"))));
+
+        assertEquals(
+                "The Phoenix Project: A Novel about IT, DevOps, and Helping Your Business Win 5th Anniversary Edition",
+                element.getText());
+
     }
 }
